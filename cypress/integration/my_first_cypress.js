4083e4fbbd9a4361923caf1f69ee6aa510c93f49
HuyenDuong123 <cogentlabs.9@sotatek.com>
2021-11-23 20:58:41 +0700

    merge commit

---
+++ cypress/integration/my_first_cypress.js
@@ -0,0 +1,13 @@
+const cypress = require("cypress")
+
+
+///<reference types="cypress" />
+
+
+describe('My First Test', () => {
+ it('google test', function(){
+cy.visit('https://www.google.com/')
+
+
+ })
+})
